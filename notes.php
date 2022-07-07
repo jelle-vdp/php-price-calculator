7955b492a8ba2f142ee69541c616af99b9cd2274
Andy Gorbould <lost@MacBook-Air.local>
2022-07-07 11:23:33 +0200

    andy test push

---
+++ notes.php
@@ -0,0 +1,33 @@
+<!-- CURRENCY FORMATTER -->
+function amountFormat(int $number, $separator = ",", $currency = "🤑€") {
+  $decimal = substr($number, -2);
+  $amount = substr($number, 0, -2) . $separator . $decimal;
+  return $currency . $amount;
+}
+<!-- call CURRENCY FORMATTER -->
+echo amountFormat(25000); // return 🤑€250,00
+<!--  -->
+
+
+page 1 - select customer
+page 2 - select products
+page 3 - show total & discounts
+
+
+
+
+
+
+
+
+
+page 1 -- only select user form drop down
+
+
+page 2 -- name & price, add to cart button? quantity button?
+
+
+prices on page 2 will be formatted for display
+
+
+USER model - 1st & last name concat --- into html dropdown
