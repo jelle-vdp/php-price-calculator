535fe18e08b29820f81cff1439a954fc31cb571b
Jelle VdP <jellevandepoel@gmail.com>
2022-07-07 10:32:16 +0200

    test commit

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
 
-    define("DB_HOST", "localhost");
-    define("DB_USER", "jelle");
-    define("DB_PASS", "shrimp123");
-    define("DB_NAME", "price-calculator");
+    require "config/connect.php";
 
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT COUNT(*) as total FROM customer";
+    $sql = "SELECT firstname FROM customer";
     $result = mysqli_query($conn, $sql);
-    $amountOfCustomers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $allCustomers = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    echo $amountOfCustomers[0]["total"];
+    var_dump($allCustomers);
