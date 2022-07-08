e92935c4a16437623610e470064eb2995535c58b
Jelle VdP <jellevandepoel@gmail.com>
2022-07-08 10:15:39 +0200

    added base controller

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+    declare(strict_types=1);
 
     require "views/includes/header.php";
     require "views/usersView.php";
     require "views/productsView.php";
     require "views/includes/footer.php";
+
+    require "controllers/BaseController.php";
+
+    $baseController = new BaseController();
+    
